75850c756b899af390085a62875f8fbdf36599c7
WoutDavid <wouters.david@hotmail.com>
2021-12-13 17:47:48 +0100

    added script to backup dotsq files

---
+++ dots.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp -u $HOME/suckless/dwm-6.2/config.h $HOME/run_commands/dwm/
+
+cp -u $HOME/.bashrc $HOME/run_commands/bashrc
+cp -u $HOME/.vimrc $HOME/run_commands/vimrc
+cp -u $HOME/.pylintrc $HOME/run_commands/pylintrc
+cp -u $HOME/.xinitrc $HOME/run_commands/xinitrc
+cp -u $HOME/.condarc $HOME/run_commands/xinitrc
+cp -ur $HOME/.config/kitty/ $HOME/config/
+cp -ur $HOME/.config/nnn/ $HOME/config/
+cp -ur $HOME/.config/nvim/ $HOME/config/
+cp -ur $HOME/.config/spicetify/ $HOME/config/
+cp -ur $HOME/.config/zathura/ $HOME/config/
