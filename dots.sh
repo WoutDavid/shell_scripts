2b0ac7bacec35fb3941a8621acdc1bc6897c35ac
WoutDavid <wouters.david@hotmail.com>
2022-01-20 10:42:15 +0100

    added snippets and removed spicetify from dots, added mimetypes script

--- dots.sh
+++ dots.sh
@@ -7,8 +7,8 @@ cp -u $HOME/.vimrc $HOME/run_commands/vimrc
 cp -u $HOME/.pylintrc $HOME/run_commands/pylintrc
 cp -u $HOME/.xinitrc $HOME/run_commands/xinitrc
 cp -u $HOME/.condarc $HOME/run_commands/xinitrc
-cp -ur $HOME/.config/kitty/ $HOME/config/
-cp -ur $HOME/.config/nnn/ $HOME/config/
-cp -ur $HOME/.config/nvim/ $HOME/config/
-cp -ur $HOME/.config/spicetify/ $HOME/config/
-cp -ur $HOME/.config/zathura/ $HOME/config/
+cp -ur $HOME/.config/kitty/ $HOME/run_commands/config/
+cp -ur $HOME/.config/nnn/ $HOME/run_commands/config/
+cp -ur $HOME/.config/nvim/ $HOME/run_commands/config/
+cp -ur $HOME/.config/zathura/ $HOME/run_commands/config/
+cp -ur $HOME/.config/nvim/coq-user-snippets $HOME/run_commands/config/
